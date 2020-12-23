ed23df779a6d0ded21e5d94e7b9749695d955c8c
Noel Melia <37183863+NoelMelia@users.noreply.github.com>
2020-12-23 22:21:03 +0000

    Web Service Working

--- web-service.py
+++ web-service.py
@@ -1,12 +1,27 @@
 # flask for web app.
 import flask as fl
-# numpy for numerical work.
-import numpy as np
+from flask import request
+import tensorflow as tf
 
 # Create a new web app.
 app = fl.Flask(__name__)
 
 # Add root route.
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
-  return current_app.send_static_file('index2.html')
+  return app.send_static_file('index.html')
+
+@app.route("/windpower", methods=['POST'])
+def wind():
+  windSpeed = float(request.get_json()["value"])
+  
+  model = tf.keras.models.load_model("model.h5")
+  
+  predict = model.predict([windSpeed])
+  
+  predlist = predict.tolist()
+  return{'predict':predlist[0]}
+
+
+if __name__ =='__main__':
+    app.run(debug=True,port=5003)

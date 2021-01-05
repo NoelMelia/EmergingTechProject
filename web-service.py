343db7fe58487693ce0de5ccf6d846673d65efcf
Noel Melia <37183863+NoelMelia@users.noreply.github.com>
2021-01-05 20:33:06 +0000

    Index File Updated with Structure

--- web-service.py
+++ web-service.py
@@ -2,6 +2,7 @@
 import flask as fl
 from flask import request
 import tensorflow as tf
+import math
 
 # Create a new web app.
 app = fl.Flask(__name__)
@@ -14,7 +15,8 @@ def home():# Get the Index Page from the static Folder
 
 @app.route("/windpower", methods=['POST'])
 def wind():
-  # Get the value and change to float type
+  # Get the value from the request method from flask and change to float type
+  # https://flask.palletsprojects.com/en/1.1.x/api/#flask.Request.get_json
   windSpeed = float(request.get_json()["value"])
   # Load the model from h5 file 
   model = tf.keras.models.load_model("model.h5")
@@ -22,12 +24,13 @@ def wind():
   predict = model.predict([windSpeed])
   # Pass the Value into a list
   predlist = predict.tolist()
+  
   # Return the Prediction Value to the Index File
   return{'predict':predlist[0]}
 
 
 if __name__ =='__main__':
   # Run the App on Port 5003
-    app.run(debug=True,port=5003)
+    app.run(debug=True,port=5001)
 
     # https://neptune.ai/blog/how-to-serve-machine-learning-models-with-tensorflow-serving-and-docker

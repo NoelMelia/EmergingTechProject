4ed316f5404e9775091477dabb36719332c05b8f
Noel Melia <37183863+NoelMelia@users.noreply.github.com>
2021-01-05 19:34:45 +0000

    Webservice Updated

--- web-service.py
+++ web-service.py
@@ -1,4 +1,4 @@
-# flask for web app.
+# Imports needed
 import flask as fl
 from flask import request
 import tensorflow as tf
@@ -9,22 +9,25 @@ app = fl.Flask(__name__)
 
 # Add root route.
 @app.route("/", methods=['GET'])
-def home():
+def home():# Get the Index Page from the static Folder
   return app.send_static_file('index.html')
 
 @app.route("/windpower", methods=['POST'])
 def wind():
+  # Get the value and change to float type
   windSpeed = float(request.get_json()["value"])
-  
+  # Load the model from h5 file 
   model = tf.keras.models.load_model("model.h5")
-  
+  # Predict the value that the user inputs
   predict = model.predict([windSpeed])
-  
+  # Pass the Value into a list
   predlist = predict.tolist()
+  # Return the Prediction Value to the Index File
   return{'predict':predlist[0]}
 
 
 if __name__ =='__main__':
+  # Run the App on Port 5003
     app.run(debug=True,port=5003)
 
     # https://neptune.ai/blog/how-to-serve-machine-learning-models-with-tensorflow-serving-and-docker
